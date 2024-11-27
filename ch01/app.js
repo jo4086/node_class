495937ea2c8a45349ad35f6507191be17b3f018a
jo4086 <ghzktm5892@gmail.com>
2024-11-27 16:44:01 +0900

    첫 커밋

---
+++ ch01/app.js
@@ -0,0 +1,6 @@
+console.log('안녕! 나의 첫 node!')
+
+function add(a, b) {
+   return a + b
+}
+console.log(add(1, 2))
---
+++ ch01/blocking.js
@@ -0,0 +1,11 @@
+
+function longRunningTask() {
+	// 오래 걸리는 작업...
+	console.log('작업끝')
+}
+
+console.log('시작')
+longRunningTask()
+console.log('다음작업')
+
+	
