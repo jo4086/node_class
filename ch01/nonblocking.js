cd22f8720231b54c3e42d677bd09f2a0262b5d14
jo4086 <ghzktm5892@gmail.com>
2024-11-28 05:31:43 +0900

    논블록킹

--- ch01/nonblocking.js
+++ ch01/nonblocking.js
@@ -1,8 +1,9 @@
 function longRunningTask() {
 	// 오래 걸리는 작업
-	console.log('작업끝')
+	console.log('첫번째 코드 실행시작')
+	console.log('첫번째 코드 작업끝')
 }
 
-console.log('시작')
+console.log('전체 코드 시작')
 setTimeout(longRunningTask,0)
-console.log('다음작업')
+console.log('두번째 코드 실행시작')
