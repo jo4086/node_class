888708bc98b280f12e3866f055569560d1b9579c
jo4086 <ghzktm5892@gmail.com>
2024-12-02 18:19:24 +0900

    Y

--- node_quiz/ch02/quiz02/main.mjs
+++ node_quiz/ch02/quiz02/main.mjs
@@ -1,5 +1,15 @@
 // stringUtils.js에서 내보낸 toUpperCase()를 가져와 아래 코드를 완성하세요.
 
-const input = 'hello world'
-const result = toUpperCase(input)
+// ECMA스크립트 방식
+import { toUpperCase2 } from './stringUtils.mjs'
+
+import toUpperCase from './stringUtils.mjs'
+
+const input1 = 'hello world / Function ~ export default ~'
+const input2 = 'hello world / export Function'
+
+const result = toUpperCase(input1)
+const result2 = toUpperCase2(input2)
+
 console.log(result) // 결과는 "HELLO WORLD"가 되어야 합니다.
+console.log(result2) // 결과는 "HELLO WORLD"가 되어야 합니다.
--- node_quiz/ch02/quiz02/stringUtils.mjs
+++ node_quiz/ch02/quiz02/stringUtils.mjs
@@ -1,1 +1,10 @@
 // 주어진 문자열을 대문자로 변환하는 함수 toUpperCase()를 내보내는 코드를 작성하세요.
+
+function toUpperCase(data) {
+    return data.toUpperCase()
+}
+export default toUpperCase
+
+export function toUpperCase2(data) {
+    return data.toUpperCase()
+}
