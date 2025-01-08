a5ec68666740007a7638da1a630140a867c31a9d
jo4086 <ghzktm5892@gmail.com>
2025-01-08 18:19:52 +0900

    25/01/08

--- shopmax/shopmax-frontend/src/App.js
+++ shopmax/shopmax-frontend/src/App.js
@@ -1,23 +1,83 @@
 import './styles/common.css'
 import { Route, Routes } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { checkAuthStatusThunk } from './features/authSlice'
+import { useEffect } from 'react'
+import { Toolbar } from '@mui/material'
 
 import Navbar from './components/shared/Navbar'
 import Footer from './components/shared/Footer'
-import Home from './pages/Home'
-import LoginPage from './pages/LoginPage'
 import SignupPage from './pages/SignupPage'
+import LoginPage from './pages/LoginPage'
+import Home from './pages/Home'
+import ItemCreatePage from './pages/ItemCreatePage'
+import ItemEditPage from './pages/ItemEditPage'
+import ItemListPage from './pages/ItemListPage'
+
+import RedirectLoginRoute from './components/auth/RedirectLoginRoute'
+import AdminRoute from './components/auth/AdminRoute'
 
 function App() {
+    const dispatch = useDispatch()
+    const { isAuthenticated, user } = useSelector((state) => state.auth) //로그인 상태, 로그인 한 사용자 정보
+
+    //새로고침시 redux state가 초기화 되거나 프로그램 실행 중 문제 발생 가능성이 있으므로 지속적인 로그인 상태 확인을 위해 사용
+    useEffect(() => {
+        dispatch(checkAuthStatusThunk())
+    }, [dispatch])
+
     return (
         <>
-            <Navbar />
+            <Navbar isAuthenticated={isAuthenticated} user={user} />
+            {/* 메뉴에 아래 컨텐츠가 가려져서 보이는 문제 해결 -> Appbar 높이만큼 여백추가 */}
+            <Toolbar />
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/singup" element={<SignupPage />} />
-                <Route path="/login" element={<LoginPage />} />
+                <Route path="/signup" element={<SignupPage />} />
+                <Route
+                    path="/login"
+                    element={
+                        // 로그인 상태일때는 home으로 리다이렉트
+                        <RedirectLoginRoute>
+                            <LoginPage />
+                        </RedirectLoginRoute>
+                    }
+                />
+
+                {/* 상품 등록 */}
+                <Route
+                    path="/items/create"
+                    element={
+                        // 관리자가 아닐경우 home으로 리다이렉트
+                        <AdminRoute>
+                            <ItemCreatePage />
+                        </AdminRoute>
+                    }
+                />
+                {/* 상품 수정 */}
+                <Route
+                    path="/items/edit/:id"
+                    element={
+                        // 관리자가 아닐경우 home으로 리다이렉트
+                        <AdminRoute>
+                            <ItemEditPage />
+                        </AdminRoute>
+                    }
+                />
+                {/* 상품리스트 */}
+                <Route
+                    path="/items/createlist"
+                    element={
+                        // 관리자가 아닐경우 home으로 리다이렉트
+                        <AdminRoute>
+                            <ItemListPage />
+                        </AdminRoute>
+                    }
+                />
             </Routes>
             <Footer />
         </>
     )
 }
+
 export default App
--- shopmax/shopmax-frontend/src/pages/LoginPage.jsx
+++ shopmax/shopmax-frontend/src/pages/LoginPage.jsx
@@ -2,11 +2,11 @@ import Login from '../components/auth/Login'
 import { Container } from '@mui/material'
 
 function LoginPage() {
-    return (
-        <Container maxWidth="md" sx={{ marginTop: 10, marginBottom: 13 }}>
-            <Login />
-        </Container>
-    )
+   return (
+      <Container maxWidth="md" sx={{ marginTop: 10, marginBottom: 13 }}>
+         <Login />
+      </Container>
+   )
 }
 
 export default LoginPage
